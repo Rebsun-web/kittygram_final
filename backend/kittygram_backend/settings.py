20b98a9ed44d36c45bbfc99ab5b0e6702f2be983
yc-user <you@example.com>
2023-08-02 18:21:47 +0000

    first review

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -1,12 +1,13 @@
 # flake8: noqa
 import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('SECRET_KEY') 
 
-DEBUG = os.environ.get('DEBUG') == 'True' 
+DEBUG = os.environ.get('DEBUG').lower() == 'true' 
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(' ') 
 
