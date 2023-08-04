f8f40048ae6ccf4bc52882fcaa655afedbdea265
yc-user <you@example.com>
2023-08-04 17:07:55 +0000

    settings

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -1,15 +1,14 @@
 # flake8: noqa
 import os
 from pathlib import Path
-from dotenv import load_dotenv
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('SECRET_KEY') 
 
-DEBUG = os.environ.get('DEBUG').lower() == 'true' 
+DEBUG = 'false' 
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(' ') 
+ALLOWED_HOSTS = 'localhost'
 
 INSTALLED_APPS = [
     'django.contrib.admin',
